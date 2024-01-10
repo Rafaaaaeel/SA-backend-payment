d5024d026934cdafa915e0f3bc29a4f44d99fc0d
Rafael <rafaelopires@icloud.com>
2024-01-09 22:02:13 -0300

    Saving files

--- PaymentApp/Dto/Create/CreateInstallmentDto.cs
+++ PaymentApp/Dto/Create/CreateInstallmentDto.cs
@@ -5,6 +5,7 @@ namespace PaymentApp.Dto.Create
         public required string Name { get; set; }
         public string? Description { get; set; }
         public decimal Value { get; set; }
+        public required decimal Total { get; set; }
         public required int Quantity { get; set; }
         public DateTime? Date { get; set; }
     }

358f2072b79a46669a36371a9e26608a9b778a48
Rafael <rafaelopires@icloud.com>
2024-02-12 15:46:49 -0300

    Saving files

--- src/Controllers/InstallmentController.cs
+++ src/Controllers/InstallmentController.cs
@@ -1,44 +1,43 @@
-namespace PaymentApp.Controllers
+namespace Sa.Payment.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+[Authorize]
+public class InstallmentsController : ControllerBase
 {
-    [Route("api/v1/[controller]")]
-    [ApiController]
-    [Authorize]
-    public class InstallmentsController : ControllerBase
+    private readonly IInstallmentsRepository _repository;
+    private readonly IMapper _mapper;
+
+    public InstallmentsController(IInstallmentsRepository repository, IMapper mapper)
     {
-        private readonly IInstallmentsRepository _repository;
-        private readonly IMapper _mapper;
-
-        public InstallmentsController(IInstallmentsRepository repository, IMapper mapper)
-        {
-            _repository = repository;
-            _mapper = mapper;
-        }
-
-        [HttpPost("{id}/installment")]
-        public async Task<ActionResult> CreateInstallment([FromBody] CreateInstallmentDto request, [FromRoute] int id)
-        {
-            Installment installment = _mapper.Map<Installment>(request);
-
-            await _repository.CreateInstallmentForCard(installment, id);
-
-            return NoContent();
-        }
-
-        [HttpDelete("{id}/installment")]
-        public async Task<ActionResult> DeleteSingleInstallment([FromRoute] int id)
-        {
-            await _repository.DeleteOccurenceInstallment(id);
-
-            return NoContent();
-        }
-
-        [HttpDelete("id")]
-        public async Task<ActionResult> DeleteManyInstallments([FromRoute] int id)
-        {
-            await _repository.DeleteInstallment(id, id);
-
-            return NoContent();
-        }
-
+        _repository = repository;
+        _mapper = mapper;
     }
+
+    [HttpPost("{id}/installment")]
+    public async Task<ActionResult> CreateInstallment([FromBody] CreateInstallmentDto request, [FromRoute] int id)
+    {
+        Installment installment = _mapper.Map<Installment>(request);
+
+        await _repository.CreateInstallmentForCard(installment, id);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}/installment")]
+    public async Task<ActionResult> DeleteSingleInstallment([FromRoute] int id)
+    {
+        await _repository.DeleteOccurenceInstallment(id);
+
+        return NoContent();
+    }
+
+    [HttpDelete("id")]
+    public async Task<ActionResult> DeleteManyInstallments([FromRoute] int id)
+    {
+        await _repository.DeleteInstallment(id, id);
+
+        return NoContent();
+    }
+
 }

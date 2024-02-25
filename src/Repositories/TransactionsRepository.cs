8fbfdeb8bfdc18fc8fb5cd4dba3f6c5e15fbd3c9
Rafael <rafaelopires@icloud.com>
2024-02-25 01:49:04 -0300

    Saving files

--- src/Repositories/TransactionsRepository.cs
+++ src/Repositories/TransactionsRepository.cs
@@ -2,13 +2,11 @@ namespace Sa.Payment.Api.Repositories;
 
 public class TransactionsRepository : ITransactionsRepository
 {
-    private readonly CardContext _context;
     private readonly IMapper _mapper;
     private readonly ICardRepository _cardRepository;
 
-    public TransactionsRepository(CardContext context, IMapper mapper, ICardRepository cardRepository)
+    public TransactionsRepository(IMapper mapper, ICardRepository cardRepository)
     {
-        _context = context;
         _mapper = mapper;
         _cardRepository = cardRepository;
     }
